78709d0c1219856df6db58ddde37dad1013204cf
Silvio <4004968+s1lvi0@users.noreply.github.com>
2025-06-05 11:30:13 +0200

    About custom

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.librenmsv1')
 
-@section('title', __('About'))
+@section('title', __('About Custom'))
 
 @section('content')
 <div class="modal fade" id="git_log" tabindex="-1" role="dialog" aria-labelledby="git_log_label" aria-hidden="true">
